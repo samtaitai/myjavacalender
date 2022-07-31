766363f40f51325a203725b9e227a64690514a78
Soyon <samtaitai@gmail.com>
2022-07-31 11:01:50 -0400

    modified FindDays class

--- src/FindDays.java
+++ src/FindDays.java
@@ -3,31 +3,40 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class FindDays {
+	
+	public List<Integer> longMonth = new ArrayList<>(List.of(1,3,5,7,8,10,12));
+	public List<Integer> shortMonth = new ArrayList<>(List.of(2,4,6,9,11));
 
-	static List<Integer> longMonth = new ArrayList<>(List.of(1,3,5,7,8,10,12));
-	static List<Integer> shortMonth = new ArrayList<>(List.of(2,4,6,9,11));
-
-	static int month;
+	public int month;
+	public int iterationNum;
 	
 	public static void FindMonthlyDays() {
 		
-		System.out.println("Input your choice of month: ");
+		FindDays fd = new FindDays();
 		
+		System.out.println("Insert the number of iteration: ");
 		Scanner scanner = new Scanner(System.in);
-		month = scanner.nextInt();
+		fd.iterationNum = scanner.nextInt();	
 		
-		System.out.println("Your choice of month is: " + month);
-		
-		if(longMonth.contains(month)) {
-			System.out.println("It has 31 days.");
-		}else if(shortMonth.contains(month)) {
-			System.out.println("It has 30 days.");
-		}else {
-			System.out.println("Your number is out of range.");
+		for(int i=0; i<fd.iterationNum; i++) {
+			
+			System.out.println("Input your choice of month: ");
+			
+			fd.month = scanner.nextInt(); 
+			
+			System.out.println("Your choice of month is: " + fd.month);
+			
+			if(fd.longMonth.contains(fd.month)) {
+				System.out.println("It has 31 days.");
+			}else if(fd.shortMonth.contains(fd.month)) {
+				System.out.println("It has 30 days.");
+			}else {
+				System.out.println("Your number is out of range.");
+			}
+			
 		}
+		scanner.close(); //make sure to close it outside for loop
 		
-		scanner.close();
 	}
-	
 	
 }
